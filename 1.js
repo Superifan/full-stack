d930458e5bb5d66778d50a06ff682d082821ff6a
Superifan <Superifan@qq.com>
2020-10-15 22:00:21 +0800

    321

--- 1.js
+++ 1.js
@@ -1,15 +1,22 @@
-import { Application, Router,send } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 
 const router = new Router();
 router
-app.use(async (context) => {
-  await send(context, "/", {
-    root: `${Deno.cwd()}/dist`,
-    index: "index.html",
+  .get("/", async (context) => {
+    await send(context, context.request.url.pathname, {
+      root: `${Deno.cwd()}/dist`,
+      index: "index.html",
+    });
+  })
+  .get("/books", (ctx) => {
+  getQuery(ctx, { mergeParams: true });
   });
-});
+
+
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+// console.log(`${Deno.cwd()}`)
 
 await app.listen({ port: 8080 });
